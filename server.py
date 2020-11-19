f47ee6d17c296e0cfa39eb2f8c39ff530512897d
Jack Cascio <jcascio2@ucmerced.edu>
2020-11-18 17:21:29 -0800

    changed names to MIGHTY_PROJECT

--- server.py
+++ server.py
@@ -9,14 +9,14 @@ import base64
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app) #look into, need it to display data? maybe?
 
 # os.environ.get returns None if the variable doesn't exist
-DATABASE = os.environ.get('STICKA_STO_DB_NAME')
-HOST = os.environ.get('STICKA_STO_DB_HOST')
-PORT = os.environ.get('STICKA_STO_DB_PORT')
-PASSWORD = os.environ.get('STICKA_STO_AD_PW')
-USER = os.environ.get('STICKA_STO_USER')
+DATABASE = os.environ.get('MIGHTY_PROJECT_DB_NAME')
+HOST = os.environ.get('MIGHTY_PROJECT_DB_HOST')
+PORT = os.environ.get('MIGHTY_PROJECT_DB_PORT')
+PASSWORD = os.environ.get('MIGHTY_PROJECT_AD_PW')
+USER = os.environ.get('MIGHTY_PROJECT_USER')
 
 conn = psycopg2.connect(
     database=DATABASE,
@@ -29,11 +29,11 @@ conn = psycopg2.connect(
 # throw error if the variables do not exist in the environment variables
 if not (DATABASE and HOST and PORT and PASSWORD and USER):
     sys.exit("""You have not set all environment variables. Check for:
-      * STICKA_STO_DB_NAME
-        * STICKA_STO_DB_HOST
-        * STICKA_STO_DB_PORT
-        * STICKA_STO_AD_PW
-        * STICKA_STO_USER """
+      * MIGHTY_PROJECT_DB_NAME
+        * MIGHTY_PROJECT_DB_HOST
+        * MIGHTY_PROJECT_DB_PORT
+        * MIGHTY_PROJECT_AD_PW
+        * MIGHTY_PROJECT_USER """
     )
 
 @app.route('/')
