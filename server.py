2081a6c90e692693243ec57f3245eab8a8925de2
Nieox <38272073+Nieox@users.noreply.github.com>
2021-01-08 11:39:09 -0800

    Updated server.py to connect to heroku postgres

--- server.py
+++ server.py
@@ -11,30 +11,42 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) #look into, need it to display data? maybe?
 
+# Local testing
 # os.environ.get returns None if the variable doesn't exist
-DATABASE = os.environ.get('MIGHTY_PROJECT_DB_NAME')
-HOST = os.environ.get('MIGHTY_PROJECT_DB_HOST')
-PORT = os.environ.get('MIGHTY_PROJECT_DB_PORT')
-PASSWORD = os.environ.get('MIGHTY_PROJECT_AD_PW')
-USER = os.environ.get('MIGHTY_PROJECT_USER')
+# DATABASE = os.environ.get('MIGHTY_PROJECT_DB_NAME')
+# HOST = os.environ.get('MIGHTY_PROJECT_DB_HOST')
+# PORT = os.environ.get('MIGHTY_PROJECT_DB_PORT')
+# PASSWORD = os.environ.get('MIGHTY_PROJECT_AD_PW')
+# USER = os.environ.get('MIGHTY_PROJECT_USER')
 
-conn = psycopg2.connect(
-    database=DATABASE,
-    host=HOST,
-    port=PORT,
-    password=PASSWORD,
-    user=USER
-)
+# conn = psycopg2.connect(
+#     database=DATABASE,
+#     host=HOST,
+#     port=PORT,
+#     password=PASSWORD,
+#     user=USER
+# )
+
+# Production 
+# os.environ for heroku credentials
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+if not (DATABASE_URL):
+    sys.exit("""You have not set all environment variables. Check for:
+      * DATABSE_URL"""
+    )
 
 # throw error if the variables do not exist in the environment variables
-if not (DATABASE and HOST and PORT and PASSWORD and USER):
-    sys.exit("""You have not set all environment variables. Check for:
-      * MIGHTY_PROJECT_DB_NAME
-        * MIGHTY_PROJECT_DB_HOST
-        * MIGHTY_PROJECT_DB_PORT
-        * MIGHTY_PROJECT_AD_PW
-        * MIGHTY_PROJECT_USER """
-    )
+# if not (DATABASE and HOST and PORT and PASSWORD and USER):
+#     sys.exit("""You have not set all environment variables. Check for:
+#       * MIGHTY_PROJECT_DB_NAME
+#         * MIGHTY_PROJECT_DB_HOST
+#         * MIGHTY_PROJECT_DB_PORT
+#         * MIGHTY_PROJECT_AD_PW
+#         * MIGHTY_PROJECT_USER """
+#     )
 
 @app.route('/')
 def hello_world():
