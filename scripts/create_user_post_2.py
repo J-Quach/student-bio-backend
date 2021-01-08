969c3a6d6f3449333eedb784713cbe823c08b99d
Nieox <38272073+Nieox@users.noreply.github.com>
2021-01-08 00:08:28 -0800

    test script

--- scripts/create_user_post_2.py
+++ scripts/create_user_post_2.py
@@ -2,11 +2,11 @@ import requests
 
 
 post_data = {
-    "firstName": "Jonah",
-    "lastName": "Nguyen",
-    "hobby": "Gamer girl",
-    "bio": "Best audition player ever",
-    "favNum": 8
+    "firstName": "Jotaro",
+    "lastName": "Star",
+    "hobby": "ORAORAORAORAORAOROARAORAORAORAORAOROA",
+    "bio": "ORAORAORAORAORAOROARAORAORAORAORAOROAORAORAORAORAORAOROARAORAORAORAORAOROAORAORAORAORAORAOROARAORAORAORAORAOROAORAORAORAORAORAOROARAORAORAORAORAOROAORAORAORAORAORAOROARAORAORAORAORAOROA",
+    "favNum": 69
 }
 
 res = requests.post('http://127.0.0.1:5000/create_user', json=post_data)
